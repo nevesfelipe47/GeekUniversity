2dd9e274d13b1674f56324f5b814f819fedf971e
nevesfelipe47 <santosfelpe47@gmail.com>
2022-12-23 08:01:08 -0300

    FIRST 1commit

--- src/VetoresEmAtrizes/Programa18.java
+++ src/VetoresEmAtrizes/Programa18.java
@@ -34,8 +34,26 @@ public class Programa18 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Declaração 
+		int outros_numeros [][];
+		
+		
+	// Declaraçao e definição de matriz	
 	int numeros[][]	= new int [3][3];
+	
+	// Declara definir o tamanho e inicializar 
+		int mais_numeros[][] = { {1,2,3}, {4,5,6}, {7,8,9}};
+	
+	//Declarar uma matriz informando somente as linhas 
+	int matriz [][]= new int [2][];
+	matriz [0] = new int [5];
+	matriz [1] = new int [3];
 
+	System.out.println("Matriz " + matriz[0][5]);
+	System.out.println(mais_numeros[0][0]);
+	System.out.println(mais_numeros[0][1]);
+	System.out.println(mais_numeros[0][2]);
+	System.out.println(mais_numeros[1][0]);
 	
 		
 	}
